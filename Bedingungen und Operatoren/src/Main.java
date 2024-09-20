a90b70d9ba9227cf09651b537aaeb15c99ec4644
Jan <jan.keckeis@hotmail.com>
2024-09-20 10:41:33 +0200

    Finished Bediningungen & Operatoren

--- Bedingungen und Operatoren/src/Main.java
+++ Bedingungen und Operatoren/src/Main.java
@@ -1,6 +1,17 @@
+package at.ran.basics;
+
+import java.util.Random;
+
+import static com.sun.beans.introspect.PropertyInfo.Name.bound;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-    int test = 123;
+    Random random = new Random();
+    int randomNumber = random.nextInt( bound : 100)
+    int randomNumber = 123;
+    if(randomNumber>50) && (randomNumber<120){
+
+    } else if(randomNumber<10)
 }
