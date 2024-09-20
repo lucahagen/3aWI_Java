c89ab2de9f55a1e8a1f4be2f906d71ec071432c8
Jan <jan.keckeis@hotmail.com>
2024-09-20 11:23:30 +0200

    Finished Swift

---
+++ Bedingungen und Operatoren/IF Bedingungen/src/ControlStructurs3.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class ControlStructurs3 {
+
+    public static void main(String[] args) {
+
+        // Erstelle eine Zufallszahl zwischen 5 und 10
+        Random random = new Random();
+        int randomNumber = random.nextInt(11 - 6 + (11));
+
+
+        switch (randomNumber) {
+            case 10:
+                System.out.println("Ten");
+                break;
+
+            case 9:
+                System.out.println("Nine");
+                break;
+
+            case 8:
+                System.out.println("Eight");
+                break;
+        }
+    }
+}
--- Bedingungen und Operatoren/IF Bedingungen/src/IF_2.java
+++ Bedingungen und Operatoren/IF Bedingungen/src/IF_2.java
@@ -1,7 +1,3 @@
-package at.ran.basics;
-
-
-
 import java.util.Random;
 
 
