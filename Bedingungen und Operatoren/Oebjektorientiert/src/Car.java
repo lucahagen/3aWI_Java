8c73a64ff8ba4a99bc303f8f6fa51652e097203b
Jan <jan.keckeis@hotmail.com>
2024-11-08 14:50:00 +0100

    Finished Project Car

--- Bedingungen und Operatoren/Oebjektorientiert/src/Car.java
+++ Bedingungen und Operatoren/Oebjektorientiert/src/Car.java
@@ -1,7 +1,35 @@
+public class Car {
+    public int fuelConsumption;
+    public int fuelAmount;
+    public String brand;
+    public String serialNumber;
+    private String color;
+    public int tankVolume;
 
-    public class Car {
-        public int fuelConsumption;
-        public String brand;
-        public String serialNumber;
-        private String color;
+    public void drive() {
+        this.fuelAmount -= fuelConsumption;
+        System.out.println("I am driving");
     }
+
+    public void breakCar() {
+        System.out.println("Ich bremse");
+    }
+
+    public void turboBoost() {
+        if (fuelAmount > tankVolume / 10) {
+            System.out.println("SuperBoostMode");
+        } else {
+            System.out.println("Not enough fuel to go to Superboost");
+        }
+    }
+
+    public void hupe(int amountOfRepetitions) {
+        for (int i = 0; i < amountOfRepetitions; i++) {
+            System.out.println("Tuuut");
+        }
+    }
+
+    public int getRemainingRange() {
+        return fuelAmount  / fuelConsumption;
+    }
+}
